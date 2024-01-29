54b38cfbd53df6239b782fe0ef5a8b895363d2c0
unknown <sophieainsworth3@gmail.com>
2024-01-29 20:43:17 +0000

    updated js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function fetchWeatherData(cityName) {
 
-  var geoLocation = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=" + apiKey;
+  var geoLocation = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=61f330dba4f57ef74ff9bbf61009b929";
 
   fetch(geoLocation)
     .then(function (response) {
@@ -9,7 +9,7 @@ function fetchWeatherData(cityName) {
     .then(function (data) {
       var cityLat = data[0].lat;
       var cityLon = data[0].lon;
-      var cityWeather = "https://api.openweathermap.org/data/2.5/forecast?lat=" + cityLat + "&lon=" + cityLon + "&appid=" + apiKey;;
+      var cityWeather = "https://api.openweathermap.org/data/2.5/forecast?lat=" + cityLat + "&lon=" + cityLon + "&appid=61f330dba4f57ef74ff9bbf61009b929";
       fetch(cityWeather)
         .then(function (response) {
           return response.json();
